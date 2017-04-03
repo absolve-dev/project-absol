79aa2c2d60bb5af3cf277e3b5e7e724f45f1ab56
Cristian Flores <cdfe.zero@gmail.com>
2017-04-03 11:23:05 -0700

    - Oops, forgot to end while loop and if statement.

--- wordpress/Advanced Custom Fields/Repeater Logic.php
+++ wordpress/Advanced Custom Fields/Repeater Logic.php
@@ -3,7 +3,7 @@
 /*
   Author: Cristian F. // Zero
   Description: Repeater logic for Advanced Custom Fields on WordPress
-  Version: 1.0
+  Version: 1.1
 */
 
 $field_group = NULL;
@@ -12,6 +12,7 @@ $post_object = NULL;
 if(have_rows($field_group)): // check if "field_group_name" has any data to show elements on a page
   while(have_rows($field_group)): the_row(); // iterate through the rows
 
+    // ignore if statement if no post object is present in the repeater
     if(get_sub_field($post_object)): // checking if theres a post object, should probably loop through repeater with while anyway... I'll try later
       $post_object = get_sub_field($post_object); // add post object to variable
       $post = $post_object; // override post
@@ -21,8 +22,11 @@ if(have_rows($field_group)): // check if "field_group_name" has any data to show
       the_post_thumbnail_url(); // get post object image url
       // theres more of these, will add as I use them
     endif;
-    wp_reset_postdata(); // reset $post for the rest of the page
+    wp_reset_postdata(); // reset $post for the rest of the page, not necessary if no post object in repeater
 
     $var1 = get_sub_field('non_object_field_name'); // add regular field data to variable
     //assumes these fields contain 1 value, if they do not a loop is required
+
+  endwhile;
+endif;
 ?>
